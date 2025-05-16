65697e44ecb76cc366be41f766b7be0375efd677
KevinPerez1234 <perezkw@students.nu-lipa.edu.ph>
2025-05-16 07:49:48 +0100

    dbs_reg

--- classes/database.php
+++ classes/database.php
@@ -1,30 +1,39 @@
-<?php 
-
-class database{
-function opencon(): PDO {
-    return new PDO (
-    'mysql:host=localhost;
-    dbname=dbs_app',
-    username: 'root',
-    password: '');
-                  
-    }   
-
-    function signupUser($first_name, $last_name, $username, $password){
-    $con = $this->opencon();
-    try {
-        $con->beginTransaction();
-        $stmt = $con->prepare("INSERT INTO Admin (admin_FN, admin_LN, 
-        admin_username, admin_password) VALUES (?,?,?,?)");
-        $stmt->execute([$first_name, $last_name, $username, $password]);
-        // Get newly inserted user_id
-        $userId = $con->lastInsertId();
-        $con->commit();
-        return $userId;
-    }catch (PDOException $e) {
-        $con->rollBack();
-        return false;
+<?php
+ 
+class database {
+ 
+    function opencon(): PDO {
+        $pdo = new PDO(
+            dsn:'mysql:host=localhost;dbname=dbs_app',
+            username: 'root',
+            password: ''
+        );
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
     }
- }
+ 
+    function signupUser($firstname, $lastname, $username, $password) {
+        $con = $this->opencon();
+ 
+        try {
+            $con->beginTransaction();
+            $stmt = $con->prepare("INSERT INTO admin (admin_FN, admin_LN, admin_username, admin_password) VALUES (?, ?, ?, ?)");
+            $stmt->execute([$firstname, $lastname, $username, $password]);
+            $userId = $con->lastInsertId();
+            $con->commit();
+            return $userId;
+        } catch (PDOException $e) {
+            $con->rollBack();
+            return false;
+        }
+    }
+ 
+    function isUsernameExists($username) {
+        $con = $this->opencon();
+        $stmt = $con->prepare("SELECT COUNT(*) FROM admin WHERE admin_username = ?");
+        $stmt->execute([$username]);
+        $count = $stmt->fetchColumn();
+        return $count > 0;
+    }
 }
-?>
+ 
